eae5f45d6b21d947a4c771d41816198de11e8a2c
theo <thjmaurino@gmail.com>
2025-01-29 15:11:53 -0600

    finished p2.1

---
+++ ps2/student_code_1.h
@@ -0,0 +1,8 @@
+#include <vector>
+
+using std::vector;
+
+vector<int> count_dishes(const vector<int>& money, const vector<int>& prices) {
+
+}
+
---
+++ ps2/student_code_2.h
@@ -0,0 +1,12 @@
+#include <vector>
+
+using std::vector;
+
+struct Price {
+    int friday, before, after;
+    Price(int friday, int before, int after): friday(friday), before(before), after(after) { }
+};
+
+long long int MinCost(const vector<Price>& giftPrices, int k) {
+
+}
